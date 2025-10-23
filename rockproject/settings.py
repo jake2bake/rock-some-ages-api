e22ac18a352a584cea3a4e53bc96a452803e89f8
Jake Littleton <jakelittleton30@gmail.com>
2025-10-23 18:43:51 -0500

    Add PostgreSQL support and RDS integration

--- rockproject/settings.py
+++ rockproject/settings.py
@@ -1,3 +1,6 @@
+
+
+
 """
 Django settings for rockproject project.
 
@@ -11,6 +14,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,8 +95,15 @@ WSGI_APPLICATION = 'rockproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.getenv('DB_NAME', 'rocksomeages'),
+    'USER': os.getenv('DB_USER', 'rockadmin'),
+    'PASSWORD': os.getenv('DB_PASSWORD'),
+    'HOST': os.getenv('DB_HOST'),
+    'PORT': os.getenv('DB_PORT', '5432'),
+     'OPTIONS': {
+            'sslmode': os.environ.get('SSLMODE', 'require'),
+        }
     }
 }
 
